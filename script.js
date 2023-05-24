77dfec5653c0d678f8722cce0d893da286e3b2e9
brandon <branthebuilder321@gmail.com>
2023-05-23 21:16:05 -0400

    Add basic functions to script, css to html

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum = 0;
+let operator = '';
+let secondNum = 0;
+
+function operate(firstNum, operator, secondNum) {
+    if (operator === '+') {
+        return add(firstNum,secondNum)
+    }
+    else if (operator === '-') {
+        return subtract(firstNum, secondNum)
+    }
+    else if (operator === '*') {
+        return multiply(firstNum, secondNum)
+    }
+    else if (operator === '/') {
+        return divide(firstNum, secondNum)
+    }
+    else {
+        return "Invalid operator"
+    }
+}
