1e618838043453807c65f88e431f101fc6870a35
brandon <branthebuilder321@gmail.com>
2023-05-24 17:24:07 -0400

    Fix chain calculations

--- script.js
+++ script.js
@@ -11,98 +11,100 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b == 0) {
+        return 'Cannot divide by zero';
+    }
     return a / b;
 }
 
-let firstNum = 0;
-let operator = '';
-let secondNum = 0;
-
 function operate(firstNum, operator, secondNum) {
     if (operator === '+') {
-        return add(firstNum,secondNum)
-    }
-    else if (operator === '-') {
+        return add(firstNum, secondNum)
+    } else if (operator === '-') {
         return subtract(firstNum, secondNum)
-    }
-    else if (operator === 'x') {
+    } else if (operator === 'x') {
         return multiply(firstNum, secondNum)
-    }
-    else if (operator === '÷') {
-        if (secondNum == 0) {
-            return 'Cannot divide by zero';
-        }
+    } else if (operator === '÷') {
         return divide(firstNum, secondNum)
-    }
-    else {
+    } else {
         return "Invalid operator"
     }
 }
 
+let calculation = {
+  firstNum: null,
+  operator: null,
+  secondNum: null
+};
+
 let displayValue = '';
+let isNewCalculation = false;
+
+function calculate() {
+  if (calculation.firstNum !== null && calculation.operator && calculation.secondNum !== null) {
+    let result = operate(parseFloat(calculation.firstNum), calculation.operator, parseFloat(calculation.secondNum));
+    
+    if (typeof result === "number") {
+        result = parseFloat(result.toFixed(2));
+    }
+    
+    displayValue = '' + result;
+    calculation.firstNum = displayValue;
+    calculation.operator = null;
+    calculation.secondNum = null;
+    displayNumber.textContent = displayValue;
+  }
+}
 
 let displayNumber = document.querySelector('.display-number');
 let numberButtons = document.querySelectorAll('.number');
 
-let isNewNumber = true;
-
 numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (button.textContent === "." && displayValue.includes('.')) {
-            return;
-        }
-
-        if (isNewNumber) {
-            displayValue = '';
-            isNewNumber = false;
-        }
-
-        displayValue += button.textContent;
-        displayNumber.textContent = displayValue;            
-    });
+  button.addEventListener('click', () => {
+    if (button.textContent === "." && displayValue.includes('.')) {
+      return;
+    }
+    if (isNewCalculation) {
+      displayValue = '';
+      isNewCalculation = false;
+    }
+    displayValue += button.textContent;
+    displayNumber.textContent = displayValue;
+    if (calculation.operator) {
+      calculation.secondNum = displayValue;
+    } else {
+      calculation.firstNum = displayValue;
+    }
+  });
 });
 
 let clearButton = document.querySelector('.clear');
+
 clearButton.addEventListener('click', () => {
-    displayValue = '';
-    displayNumber.textContent = displayValue;
+  calculation.firstNum = null;
+  calculation.operator = null;
+  calculation.secondNum = null;
+  displayValue = '';
+  displayNumber.textContent = displayValue;
+  isNewCalculation = false;
 });
 
 let operatorButtons = document.querySelectorAll('.operator');
 
 operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (firstNum && operator) {
-            secondNum = parseFloat(displayValue);
-            let result = operate(parseFloat(firstNum), operator, secondNum);
-
-            result = parseFloat(result.toFixed(2));
-            displayValue = '' + result;
-            displayNumber.textContent = displayValue;
-
-            firstNum = result;
-            operator ='';
-        } else {
-            firstNum = displayValue;
-            operator = button.textContent;  
-            isNewNumber = true;     
-        }
-    });
+  button.addEventListener('click', () => {
+    if (calculation.firstNum !== null && calculation.operator && calculation.secondNum !== null) {
+      calculate();
+      isNewCalculation = true;
+    }
+    calculation.operator = button.textContent;
+    displayValue = '';
+  });
 });
 
 let equalsButton = document.querySelector('.equals');
 
 equalsButton.addEventListener('click', () => {
-    if (firstNum && operator) {
-        secondNum = parseFloat(displayValue);
-        let result = operate(parseFloat(firstNum), operator, secondNum);
-
-        result = parseFloat(result.toFixed(2));
-        displayValue = '' + result;
-        displayNumber.textContent = displayValue;
-
-        firstNum = result;
-        operator = '';
-    }
-    isNewNumber = true;
+  calculate();
+  isNewCalculation = true;
 });
